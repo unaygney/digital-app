ad6b6068a19d65d2b58703f86af942b39ac3068f
Guney Unay <guneymarketing17@hotmail.com>
2024-07-26 18:29:04 +0300

    :passport_control: created user func

---
+++ src/app/(login)/sign-up/actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { db } from "@/db";
+import { signUpSchema } from "@/lib/validations";
+import bcrypt from "bcrypt";
+
+export async function create(data: {
+  email: string;
+  password: string;
+  terms: boolean;
+}) {
+  try {
+    let isValid = signUpSchema.safeParse(data);
+
+    if (!isValid.success) {
+      return { errors: isValid.error.flatten().fieldErrors };
+    }
+
+    // all emails should be lowercase to avoid duplicates
+    data.email = data.email.toLowerCase();
+
+    // Check if email is already in use
+    let user = await db.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (user) {
+      return { errors: "Email is already in use" };
+    }
+
+    // hast to password
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(data.password, salt);
+
+    // Create user
+    let newUser = await db.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    return { message: "User created successfully" };
+  } catch (e) {}
+}

ea92a5379875b1070e41b38b85e6a2f7186a40a4
Guney Unay <guneymarketing17@hotmail.com>
2024-07-26 18:27:34 +0300

    :safety_vest: added signupSchema validation

---
+++ src/lib/validations.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(64, "Password must be at most 64 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[^a-zA-Z0-9]/,
+      "Password must contain at least one special character",
+    ),
+  terms: z.boolean().refine((value) => value === true, {
+    message: "You must agree to the terms and conditions",
+  }),
+});
+
+export type SignUpFormData = z.infer<typeof signUpSchema>;

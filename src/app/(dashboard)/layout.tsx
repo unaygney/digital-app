28a621de4730d2e58b293fa957c00175bbba630c
Guney Unay <guneymarketing17@hotmail.com>
2024-07-27 15:43:56 +0300

    :art: created layouts

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import "../globals.css";
+import { mainFont } from "@/lib/font";
+import { Toaster } from "@/components/ui/toaster";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen scroll-smooth font-sans antialiased",
+          mainFont,
+        )}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}

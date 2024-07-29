fee9ef2ab71e2eeb2d493360a702068b88bbc24c
Guney Unay <guneymarketing17@hotmail.com>
2024-07-29 18:41:53 +0300

    :card_file_box: crud process

--- src/app/(dashboard)/actions.ts
+++ src/app/(dashboard)/actions.ts
@@ -12,3 +12,40 @@ export const getUserImages = async (email: string) => {
 
   return userWithImages?.images;
 };
+export const makeProfileImage = async (email: string, url: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { images: true, profileImage: true },
+  });
+
+  if (user?.profileImage === url)
+    return { message: "Profile image already set" };
+
+  if (!user) return { message: "User not found" };
+
+  await db.user.update({
+    where: { email },
+    data: {
+      profileImage: url,
+    },
+  });
+
+  return { message: "Profile image updated" };
+};
+export const deletePhoto = async (email: string, url: string) => {
+  console.log("deletePhoto", email, url);
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { images: true },
+  });
+
+  if (!user) return { message: "User not found" };
+
+  const deleteImage = await db.image.delete({
+    where: {
+      originalUrl: url,
+    },
+  });
+
+  return { message: "Photo deleted" };
+};

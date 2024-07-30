4ccb3a8c00b5408b1fc410e338bc4ea1b7730b29
Guney Unay <guneymarketing17@hotmail.com>
2024-07-30 18:11:31 +0300

    :wrench: update actions.ts

--- src/app/(dashboard)/actions.ts
+++ src/app/(dashboard)/actions.ts
@@ -1,27 +1,42 @@
 "use server";
 
 import { db } from "@/db";
+import { verifyJwtToken } from "@/lib/auth";
+import { cookies } from "next/headers";
 
-export const getUserImages = async (email: string) => {
+export const getTokenAndVerify = async (): Promise<string> => {
+  const cookiesStore = cookies();
+  const token = cookiesStore.get("token")?.value ?? "";
+
+  const isValidToken = await verifyJwtToken(token);
+  if (!isValidToken || !isValidToken.email) throw new Error("Invalid User");
+
+  return isValidToken.email as string;
+};
+
+export const getUserImages = async () => {
+  const email = await getTokenAndVerify();
+
   const userWithImages = await db.user.findUnique({
     where: { email },
     select: { images: true },
   });
 
-  if (!userWithImages) [];
+  if (!userWithImages) return [];
 
-  return userWithImages?.images;
+  return userWithImages.images;
 };
-export const makeProfileImage = async (email: string, url: string) => {
+export const makeProfileImage = async (url: string) => {
+  const email = await getTokenAndVerify();
+
   const user = await db.user.findUnique({
     where: { email },
     select: { images: true, profileImage: true },
   });
 
-  if (user?.profileImage === url)
-    return { message: "Profile image already set" };
-
   if (!user) return { message: "User not found" };
+  if (user.profileImage === url)
+    return { message: "Profile image already set" };
 
   await db.user.update({
     where: { email },
@@ -32,8 +47,9 @@ export const makeProfileImage = async (email: string, url: string) => {
 
   return { message: "Profile image updated" };
 };
-export const deletePhoto = async (email: string, url: string) => {
-  console.log("deletePhoto", email, url);
+export const deletePhoto = async (url: string) => {
+  const email = await getTokenAndVerify();
+
   const user = await db.user.findUnique({
     where: { email },
     select: { images: true },
@@ -41,11 +57,40 @@ export const deletePhoto = async (email: string, url: string) => {
 
   if (!user) return { message: "User not found" };
 
-  const deleteImage = await db.image.delete({
+  await db.image.delete({
     where: {
       originalUrl: url,
     },
   });
 
   return { message: "Photo deleted" };
+};
+export const handleCroppedImage = async (
+  data: {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+  },
+  url: string,
+) => {
+  const email = await getTokenAndVerify();
+
+  const user = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) return { message: "User not found" };
+
+  await db.image.update({
+    where: { originalUrl: url },
+    data: {
+      cropHeight: data.height,
+      cropWidth: data.width,
+      cropX: data.x,
+      cropY: data.y,
+    },
+  });
+
+  return { message: "Cropped image updated" };
 };

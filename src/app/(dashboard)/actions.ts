70799ad42326fb711051996ab220a00fe9f2978c
Guney Unay <guneymarketing17@hotmail.com>
2024-08-04 15:33:34 +0300

    :hammer: update actions.ts

--- src/app/(dashboard)/actions.ts
+++ src/app/(dashboard)/actions.ts
@@ -3,7 +3,11 @@
 import { db } from "@/db";
 import { getTokenAndVerify } from "@/lib/auth";
 import bcrypt from "bcrypt";
-import { accountSettingsSchema } from "@/lib/validations";
+import {
+  accountSettingsSchema,
+  NotificationsSettingsFormData,
+} from "@/lib/validations";
+type UserPreferences = NotificationsSettingsFormData;
 
 export const getUser = async () => {
   const email = await getTokenAndVerify();
@@ -168,4 +172,68 @@ export const changePassword = async (data: {
   });
 
   return { message: "Password updated" };
+};
+export const getPreferences =
+  async (): Promise<NotificationsSettingsFormData> => {
+    const email = await getTokenAndVerify();
+
+    const user = await db.user.findUnique({
+      where: { email },
+      select: { preferences: true },
+    });
+
+    if (!user || !user.preferences) {
+      return {
+        comments: { email: false, push: false, sms: false },
+        features: { email: false, push: false, sms: false },
+        friend_requests: { email: false, push: false, sms: false },
+        friend_updates: { email: false, push: false, sms: false },
+        marketing: { email: false, push: false, sms: false },
+      };
+    }
+
+    const preferences: UserPreferences = user.preferences as UserPreferences;
+
+    return {
+      comments: preferences.comments || {
+        email: false,
+        push: false,
+        sms: false,
+      },
+      features: preferences.features || {
+        email: false,
+        push: false,
+        sms: false,
+      },
+      friend_requests: preferences.friend_requests || {
+        email: false,
+        push: false,
+        sms: false,
+      },
+      friend_updates: preferences.friend_updates || {
+        email: false,
+        push: false,
+        sms: false,
+      },
+      marketing: preferences.marketing || {
+        email: false,
+        push: false,
+        sms: false,
+      },
+    };
+  };
+
+export const updatePreferences = async (
+  data: NotificationsSettingsFormData,
+) => {
+  const email = await getTokenAndVerify();
+
+  await db.user.update({
+    where: { email },
+    data: {
+      preferences: data,
+    },
+  });
+
+  return { message: "Preferences updated" };
 };

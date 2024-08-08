1593c914a6897d2ab722aa25bb697563c2fcc1f0
Guney Unay <guneymarketing17@hotmail.com>
2024-08-08 20:21:18 +0300

    :wrench: added utils func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import moment from "moment";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,3 +10,7 @@ export const toBase64 = (str: string) =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
     : window.btoa(str);
+
+export function formatDate(date: Date) {
+  return moment(date).format("Do MMMM, YYYY");
+}

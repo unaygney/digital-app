1767ab5828daa23bf5f71d4484b1439db83825e5
Guney Unay <guneymarketing17@hotmail.com>
2024-08-12 16:45:54 +0300

    :wrench: added webhook to payment status change

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.text();
+    const signature = req.headers.get("stripe-signature");
+
+    if (!signature) {
+      return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
+    }
+
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!,
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object as Stripe.Checkout.Session;
+
+      console.log(session);
+
+      const response = NextResponse.json({ success: true });
+      return response;
+    }
+  } catch (e) {
+    return NextResponse.json(
+      { error: "Webhook handler failed" },
+      { status: 400 },
+    );
+  }
+}

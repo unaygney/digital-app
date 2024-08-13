28bd019067abe471fdc96f4f36adbbd94882910a
Guney Unay <guneymarketing17@hotmail.com>
2024-08-13 20:00:11 +0300

    :wrench: added utils func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,3 +14,12 @@ export const toBase64 = (str: string) =>
 export function formatDate(date: Date) {
   return moment(date).format("Do MMMM, YYYY");
 }
+export function getInitials(
+  firstName?: string | null,
+  lastName?: string | null,
+) {
+  const firstInitial = firstName ? firstName.charAt(0).toUpperCase() : "";
+  const lastInitial = lastName ? lastName.charAt(0).toUpperCase() : "";
+
+  return firstInitial + lastInitial || "CN";
+}

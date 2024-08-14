0b793691e758f62f78ab7df736ea69f2ee78c335
Guney Unay <guneymarketing17@hotmail.com>
2024-08-14 18:17:35 +0300

    :hammer: added sendMessage func

--- src/app/(dashboard)/actions.ts
+++ src/app/(dashboard)/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 import { cookies } from "next/headers";
 
@@ -6,4 +7,14 @@ export const logout = async () => {
   const token = cookies().delete("token");
 
   return { message: "success" };
+};
+export const sendMessage = async (message: string) => {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  let promt = message;
+
+  const result = await model.generateContent(promt);
+
+  console.log(result.response.text());
 };

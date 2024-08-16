ad1ca92cecba8068b50b10c294a1e75cb2d6b20c
Guney Unay <guneymarketing17@hotmail.com>
2024-08-16 15:54:01 +0300

    :hammer: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import type { PluginAPI } from "tailwindcss/types/config";
 
 const config = {
   darkMode: ["class"],
@@ -63,22 +64,32 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        loading: {
+          "0%, 80%, 100%": { opacity: "0" },
+          "40%": { opacity: "1" },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "loading-dot": "loading 1.4s infinite ease-in-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }: { addUtilities: PluginAPI["addUtilities"] }) {
+      addUtilities({
+        ".animation-delay-0": {
+          "animation-delay": "0s",
+        },
+        ".animation-delay-200": {
+          "animation-delay": "0.2s",
+        },
+        ".animation-delay-400": {
+          "animation-delay": "0.4s",
+        },
+      });
+    },
+  ],
 } satisfies Config;
 
 export default config;
